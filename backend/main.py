3f652750e658383a8449aa9226511443886d4f79
matteolucahomsy <matteoluca.homsy@net.usj.edu.lb>
2026-05-20 10:35:42 +0300

    modified backend and frontend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app=FastAPI()
 
@@ -10,10 +11,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+class ChatRequest(BaseModel):
+    message: str
 
 @app.get("/")
 def home():
     return {"message": "Backend Running"}
-@app.get("/chat")
-def chat():
-    return{"response": "Hello from FastAPI"}
+
+@app.post("/chat")
+def chat(req: ChatRequest):
+    user_message=req.message
+    response= f"AI received: {user_message}"
+    return{"response": response}
--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,20 +1,37 @@
 import {useEffect,useState} from "react";
+import axios from "axios";
 function App() {
   const [message,setMessage] = useState("");
-  useEffect(()=>{
-    fetch("http://127.0.0.1:8000/chat")
-    .then((response)=> response.json())
-    .then((data)=> {
-      setMessage(data.response);
-    })
-    .catch((error)=>{
-      console.error("Error:",error);
+  const[chat, setChat]= useState([]);
+  const sendMessage= async() => {
+    if (!message) return ;
+    const userMsg= message;
+    setChat((prev) => [...prev,{role: "user", text: userMsg}]);
+    setMessage("");
+    const res = await axios.post("http://127.0.0.1:8000/chat",{
+      message: userMsg,
     });
-  }, []);
+    setChat((prev) => [
+      ...prev,
+      {role: "ai", text: res.data.response},
+    ]);
+  };
   return(
-    <div>
+    <div style={{padding: 20}}>
       <h1>AI Study Assistant</h1>
-      <h2>{message}</h2>
+      
+      <div>
+        {chat.map((c, i)=> (
+          <p key={i}>
+            <b>{c.role}:</b> {c.text}
+          </p>
+        ))}
+      </div>
+      <input 
+      value={message}
+      onChange={(e) => setMessage(e.target.value)}
+      placeholder="Type message ..." />
+      <button onClick={sendMessage}>Send</button>
     </div>
   );
 }
